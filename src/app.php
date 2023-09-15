ed3b8646879d7318da4c5018a9f56b58c898f921
soegaeni <szogyenyid@gmail.com>
2023-09-15 19:11:56 +0200

    Use DIContainer to resolve dependencies

--- src/app.php
+++ src/app.php
@@ -2,11 +2,23 @@
 
 namespace Szogyenyid\PhpDiExample;
 
+use DI\Definition\Exception\InvalidDefinition;
 use Szogyenyid\PhpDiExample\Classes\App;
 use Szogyenyid\PhpDiExample\Classes\Greeters\Helloer;
-use Szogyenyid\PhpDiExample\Classes\Writers\Echoer;
+use Szogyenyid\PhpDiExample\Classes\Writers\Printer;
+use Szogyenyid\PhpDiExample\Interfaces\GreeterInterface;
+use Szogyenyid\PhpDiExample\Interfaces\WriterInterface;
 
 include __DIR__ . '/../vendor/autoload.php';
 
-$app = new App((new Helloer((new Echoer()))));
-$app->run("szogyenyid");
+$container = new \DI\Container([
+    GreeterInterface::class => \DI\autowire(Helloer::class),
+    WriterInterface::class => \DI\autowire(Printer::class),
+]);
+
+try {
+    $app = $container->get(App::class);
+    $app->run("szogyenyid");
+} catch (InvalidDefinition $e) {
+    echo "Invalid definition: " . $e->getMessage();
+}
