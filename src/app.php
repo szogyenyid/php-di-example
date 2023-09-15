eb1f4bac1f1819695378c9d58bfdaf1701e1735d
soegaeni <szogyenyid@gmail.com>
2023-09-15 17:50:42 +0200

    Make it a bit more customizable

--- src/app.php
+++ src/app.php
@@ -2,15 +2,29 @@
 
 namespace Szogyenyid\PhpDiExample;
 
+use InvalidArgumentException;
+
 include __DIR__ . '/../vendor/autoload.php';
 
 class App
 {
+    public function __construct(
+        private string $function = 'echo',
+        private string $greeting = 'Hello',
+        private string $name = 'World'
+    ) {
+    }
     public function run()
     {
-        echo 'Hello World!';
+        if ($this->function === 'echo') {
+            echo $this->greeting . ' ' . $this->name . '!' . PHP_EOL;
+        } else {
+            throw new InvalidArgumentException('Invalid function');
+        }
     }
 }
 
-$app = new App();
+$app = new App(
+    name: "szogyenyid"
+);
 $app->run();
